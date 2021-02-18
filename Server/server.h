c441271baa7b2f6d2947f65dd0ae0f91e23f21d3
OOFJDL <oofjdl@inf.elte.hu>
2021-02-18 12:12:10 +0100

    Basic server setup

---
+++ Server/server.h
@@ -0,0 +1,29 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QNetworkInterface>
+#include <QAbstractSocket>
+#include <QDebug>
+
+class Server : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Server(QObject *parent = nullptr);
+
+private:
+    void initServer();
+
+    QTcpServer* _tcpServer;
+
+private slots:
+    void startConnection();
+
+signals:
+
+};
+
+#endif // SERVER_H
