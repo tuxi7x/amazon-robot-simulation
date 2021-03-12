e3295bbe5eb3d60550d136febfca3972c933a9b0
OOFJDL <oofjdl@inf.elte.hu>
2021-03-12 22:59:23 +0100

    Added Controller fields

--- Server/Controller.h
+++ Server/Controller.h
@@ -2,12 +2,30 @@
 #define CONTROLLER_H
 
 #include <QObject>
+#include <QVector>
+#include "models/Robot.h"
+#include "models/Docker.h"
+#include "models/DropOffPoint.h"
+#include "models/Product.h"
+#include "models/Shelf.h"
+#include <QTimer>
+
+
 
 class Controller : public QObject
 {
     Q_OBJECT
 public:
     explicit Controller(QObject *parent = nullptr);
+
+private:
+    QVector<Robot*> _robots;
+    QVector<Docker*> _dockers;
+    QVector<DropOffPoint*> _dropOffPoints;
+    QVector<Shelf*> _shelves;
+    QVector<Product*> _products;
+    QVector<QString> _orders;
+
 
 signals:
 
