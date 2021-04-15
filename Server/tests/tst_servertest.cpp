594a122068fc78261db03a591c65fc697945ac13
Várszegi Márk <gobc23@inf.elte.hu>
2021-04-15 14:00:48 +0200

    Initial server tests

---
+++ Server/tests/tst_servertest.cpp
@@ -0,0 +1,37 @@
+#include <QtTest>
+#include "../Controller.h"
+
+// add necessary includes here
+
+class ServerTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    ServerTest();
+    ~ServerTest();
+
+private slots:
+    void test_case1();
+
+};
+
+ServerTest::ServerTest()
+{
+
+}
+
+ServerTest::~ServerTest()
+{
+
+}
+
+void ServerTest::test_case1()
+{
+    QVERIFY(1 != 2);
+    QVERIFY(true);
+}
+
+QTEST_APPLESS_MAIN(ServerTest)
+
+#include "tst_servertest.moc"
