5ae7590f755cf4980ec90966081ea93fd94bd70f
Várszegi Márk <gobc23@inf.elte.hu>
2021-04-19 12:29:18 +0200

    Added client tests

--- Client/tests/tst_testclient.cpp
+++ Client/tests/tst_testclient.cpp
@@ -12,7 +12,12 @@ public:
     ~TestClient();
 
 private slots:
-    void test_case1();
+    void test_size();
+    void test_addRobot();
+    void test_addDocker();
+    void test_addShelf();
+    void test_addProduct();
+    void test_testSelection();
 
 };
 
@@ -26,11 +31,54 @@ TestClient::~TestClient()
 
 }
 
-void TestClient::test_case1()
+void TestClient::test_size()
 {
     MapEditorController m;
     m.createNewMap(10);
     QVERIFY(m.getSize() == 10);
+}
+
+void TestClient::test_addRobot()
+{
+    MapEditorController m;
+    m.createNewMap(10);
+    m.addRobot(1,1);
+    QVERIFY(m.getField(1,1).first == MapEditorController::Robot);
+}
+
+void TestClient::test_addDocker()
+{
+    MapEditorController m;
+    m.createNewMap(10);
+    m.addDocker(1,1);
+    QVERIFY(m.getField(1,1).first == MapEditorController::Docker);
+}
+
+void TestClient::test_addShelf()
+{
+    MapEditorController m;
+    m.createNewMap(10);
+    m.addShelf(1,1);
+    QVERIFY(m.getField(1,1).first == MapEditorController::Shelf);
+}
+
+void TestClient::test_addProduct()
+{
+    MapEditorController m;
+    m.createNewMap(10);
+    m.addShelf(1,1);
+    m.addProduct(1,1,"alma");
+    QString a = m.getProductsOnShelf(1,1).at(0);
+    QVERIFY(a == "alma");
+}
+
+void TestClient::test_testSelection()
+{
+    MapEditorController m;
+    m.createNewMap(10);
+    m.addShelf(1,1);
+    m.toggleShelfSelection(qobject_cast<ShelfFieldModel*>(m.getField(1,1).second));
+    QVERIFY(m.isASelectedShelf(1,1));
 }
 
 QTEST_APPLESS_MAIN(TestClient)
